7f049340bee9a84c8d6ede9bb60ce0208daa613d
liugh2 <liugh2@adnonstop.com>
2018-10-29 15:05:38 +0800

    Add demo:类释放内存.

---
+++ 类释放内存/类释放内存/main.swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  类释放内存
+//
+//  Created by Beauty-jishu on 2018/10/29.
+//  Copyright © 2018年 码叔. All rights reserved.
+//
+
+import Foundation
+
+class Happy {
+    // alloc memory
+    var p = UnsafeMutablePointer<Int>.allocate(capacity: 100)
+    deinit {
+        // free memory
+        self.p.deallocate()
+        print("I was released!")
+    }
+}
+
+var happy:Happy! = Happy()
+happy = nil
